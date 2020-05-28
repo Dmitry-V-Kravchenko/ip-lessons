5a165dc4f8ae4d9582daf0d67718fa261be107b5
Dmitry-V-Kravcenko <dmikrav@yandex.ru>
2020-05-28 15:30:40 +0300

    subtask 1,2 task 4 completed

--- my_tests/script.js
+++ my_tests/script.js
@@ -13,6 +13,36 @@ let {nameP, age} = person;
 
 // console.log('education level' in person);
 
-for (let key in person) {
+/* for (let key in person) {
     console.log(`${key}: ${person[key]}`);
 }
+ */
+const directivity = {
+    course: 'JavaScript',
+    tools: 'vs-code'
+};
+
+Object.assign(person, directivity);   // слить два объекта в один
+// console.log(person);
+
+const clone = Object.assign({}, directivity);  // создать поверхностный клон объекта
+const clone2 = {...directivity};
+const clone3 = JSON.parse(JSON.stringify(directivity)); // сделать глубокое клонирование
+clone.course = 'React';
+clone2.course = 'Node.js';
+clone3.course = 'MongoDB';
+
+console.log(clone);
+console.log(clone2);
+console.log(clone3);
+
+const arr = [25, 9, 67, 14];
+const arrClone1 = arr.slice();  // клонирование массивов
+const arrClone2 = [...arr];
+arrClone1[1] = 658;
+arrClone2[1] = 211;
+
+
+console.log(arr);
+console.log(arrClone1);
+console.log(arrClone2);
---
+++ new_task4/js/script.js
@@ -0,0 +1,70 @@
+'use strict';
+
+const personalMovieDB = {
+    count: 0,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+    start: function() {
+        this.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+        while (this.count == '' || this.count == null || isNaN(this.count) ) {
+            this.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+        }
+    },
+    setPersonalFilms: function() {
+        for (let i = 0; i < 2; i++) {
+            const a = prompt('Один из последних просмотренных фильмов?', ''),
+                  b = prompt('На сколько оцените его?', '');
+            if (a != null && b != null && a != '' && b!= '' && a.length < 50) {
+                this.movies[a] = b;
+                console.log('done');
+            } else {
+                i--;
+                console.log('error');
+            }
+        }
+    },
+    writeYourGenres: function () {
+        for (let i = 1; i < 4; i++) {
+            this.genres[i-1] = prompt(`Ваш любимый жанр ${i}`, '');
+        }
+    },
+    showPersonStatus: function () {
+        if (this.count < 10) {
+            console.log("Просмотрено довольно мало фильмов");
+        } else if (this.count >= 10 && this.count < 30) {
+            console.log("Вы классический зритель");
+        } else if (this.count >= 30) {
+            console.log("Вы киноман");
+        } else {
+            console.log("Произошла ошибка");
+        }
+    },
+    showMyDB: function () {
+        if (this.privat === false) {
+            for (let key in this) {
+                if (this.hasOwnProperty(key)) {
+                    console.log(`${key}: ${this[key]}`);
+                }
+            }
+        }
+    },
+    toggleVisibleMyDB: function() {
+        if (this.privat === false) {
+            this.privat = true;
+        } else {
+            this.privat = false;
+        }
+    },
+};
+
+personalMovieDB.start();
+// personalMovieDB.setPersonalFilms();
+// personalMovieDB.writeYourGenres();
+// personalMovieDB.showPersonStatus();
+personalMovieDB.showMyDB();
+
+
+console.log(personalMovieDB);
