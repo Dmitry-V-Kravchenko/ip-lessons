c6c2a6737c47e048926b49a588a94cb1c08ae7d2
Dmitry Kravchenko <dmikrav@yandex.ru>
2020-06-18 17:22:48 +0300

    task 7 modal completed

--- new_task7/js/script.js
+++ new_task7/js/script.js
@@ -1,6 +1,6 @@
 window.addEventListener('DOMContentLoaded', function() {
 
-    // Tabs
+    // Tabs ================================================================
     
 	let tabs = document.querySelectorAll('.tabheader__item'),
 		tabsContent = document.querySelectorAll('.tabcontent'),
@@ -39,7 +39,7 @@ window.addEventListener('DOMContentLoaded', function() {
 		}
     });
     
-    // Timer
+    // Timer ===================================================================
 
     const deadline = '2020-07-16';
 
@@ -94,19 +94,22 @@ window.addEventListener('DOMContentLoaded', function() {
 
     setClock('.timer', deadline);
 
-    // modal
+    // modal =========================================================
 
     const modalTrigger = document.querySelectorAll('[data-modal]'),
           modal = document.querySelector('.modal'),
           modalCloseBtn = document.querySelector('[data-close]');
 
     modalTrigger.forEach((btn) => {
-        btn.addEventListener('click', () => {
-            modal.classList.add('show');
-            modal.classList.remove('hide');
-            document.body.style.overflow = 'hidden';
-        });
+        btn.addEventListener('click', openModal);
     });
+
+    function openModal() {
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        document.body.style.overflow = 'hidden';
+        clearInterval(modalTimerId);
+    }
 
     function closeModal() {
         modal.classList.add('hide');
@@ -128,6 +131,17 @@ window.addEventListener('DOMContentLoaded', function() {
         }
     });
 
+    const modalTimerId = setTimeout(openModal, 5000);
 
+    function showModalByScroll() {
+        // Если прокрученная часть + видимый экран клиента >= всей высоты сайта, то он долистал страницу до конца
+        if ( window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+            openModal();
+            window.removeEventListener('scroll', showModalByScroll);
+        }
+    }
+
+
+    window.addEventListener('scroll', showModalByScroll);
 
 });
