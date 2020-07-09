b33b3f8cc9997a0dd57b409c364a629e368db004
Dmitry Kravchenko <dmikrav@yandex.ru>
2020-07-09 23:10:33 +0300

    task 7 food json-server

--- new_task7/js/script.js
+++ new_task7/js/script.js
@@ -289,6 +289,10 @@ window.addEventListener('DOMContentLoaded', function() {
 
     }
 
+    fetch('http://localhost:3000/menu')
+        .then(data => data.json())
+        .then(res => console.log(res));
+
 
 
 });
